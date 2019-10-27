2efee6ee478765d0d118a365374809bf5c03a89b
황종옥 <jongock@hwangjong-og-ui-MacBookPro.local>
2019-10-27 14:41:16 +0900

    sequelize sync function add

--- sever/app.js
+++ sever/app.js
@@ -4,6 +4,14 @@ var cors = require("cors")
 
 
 const app = express();
+const models = require("./models/index.js");
+
+models.sequelize.sync().then( () => {
+  console.log(" DB 연결 성공");
+}).catch(err => {
+  console.log("연결 실패");
+  console.log(err);
+})
 
 app.use(cors());
 app.use(bodyparser.json());
