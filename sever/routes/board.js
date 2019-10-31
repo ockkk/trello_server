0cc73a7faec3a43a18b91a376f980aecc1a78a16
황종옥 <jongock@hwangjong-og-ui-MacBookPro.local>
2019-10-31 11:55:40 +0900

    boards update API complete

--- sever/routes/board.js
+++ sever/routes/board.js
@@ -20,20 +20,40 @@ router.get("/list", async (req, res) => {
 
 router.post("/add", async (req,res) => {
   const userKey = await users
-  .findOne({ 
-    attributes: ["u_key"],
-    where: {u_email: req.token.email}})
-  .then(result => result.dataValues.u_key)
+    .findOne({ 
+      attributes: ["u_key"],
+      where: {u_email: req.token.email}})
+    .then(result => result.dataValues.u_key)
 
   boards
     .create({
       b_name : req.body.b_name,
       u_key : userKey
     })
-    .then(() => res.json({
+    .then(val => res.json({
+      b_key:val.dataValues.b_key,
       success: true,
       message: 'board가 생성되었습니다.📄',
     }))
+    .catch((err) => {
+      res.send(400).json({
+        success: false,
+        message: '잘못된 요청입니다.😡'
+      })
+    })
 })
 
+router.post("/delete", async (req, res) =>{
+  const userKey = await users
+  .findOne({ 
+    attributes: ["u_key"],
+    where: {u_email: req.token.email}})
+  .then(result => result.dataValues.u_key)
+
+  boards.destroy({
+    where:{
+      b_key: req.body.b_key
+    }
+  })
+})
 module.exports = router
