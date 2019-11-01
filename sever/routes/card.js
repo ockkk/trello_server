16dfad3799201c5bd0959181c0c289f4ff080160
황종옥 <jongock@hwangjong-og-ui-MacBookPro.local>
2019-11-01 11:27:13 +0900

    CRUD API set

---
+++ sever/routes/card.js
@@ -0,0 +1,103 @@
+var express = require("express");
+var router = express.Router();
+
+var contaners = require("../../models").contaners
+var cards = require("../../models").cards
+
+router.get("/:id", async (req,res) => {
+  cards
+    .findAll()
+    .then(result => res.json(result))
+})
+
+router.get("/:id", async (req, res) => {
+  cards
+    .findAll({
+      where: {ct_key: req.params.id}
+    })
+    .then(result => res.json(result))
+    .catch(err => res.send(err))
+})
+
+router.post("/:id", async (req,res) => {
+  contaners
+    .create({
+      cd_name: req.body.cd_name,
+      ct_key: req.params.ct_key
+    })
+    .then(val => res.json({
+      cd_key: val.dataValues.cd_key,
+      success: true,
+      message: 'card가 생성되었습니다.🃏',
+    }))
+    .catch((err) => { 
+      res.send(400).json({
+        success: false,
+        message: '잘못된 요청입니다.😡'
+      })
+    })
+})
+
+router.delete("/:id", async (req, res) =>{
+  let cdExits = await contaners.findOne({
+    where:{
+      cd_key: req.params.id
+    }
+  })
+
+  if(req.token && cdExits){
+    contaners
+      .destroy({
+        where:{
+          cd_key: req.params.id
+        }
+      })
+      .then(() => {
+        res.json({
+          success: true,
+          message: 'card가 삭제 되었습니다.🗑'
+        })
+      })
+      .catch((err) => {
+        res.json({
+          success: false,
+          message: '잘못된 요청입니다.😡'
+        })
+      })
+  }
+  res.send(400).json({
+    success: false,
+    message: '잘못된 요청입니다.😡'
+  })
+})
+
+router.put("/:id", async (req,res) => {
+  let cdExist = await contaners.findOne({
+    where:{
+      cd_key: req.params.id
+    }
+  })
+
+  if(req.token && cdExist){
+    await contaners.update({
+      cd_name: req.body.b_name
+    },
+    {
+      where : {
+        cd_key: req.params.id
+      }
+    })
+    .then(() => {
+      res.json({
+        success: true,
+        message: 'card가 수정 되었습니다.🃏'
+      })
+    })
+  }
+  res.send(400).json({
+    success: false,
+    message: '잘못된 요청입니다.😡'
+  })
+})
+
+module.exports = router
