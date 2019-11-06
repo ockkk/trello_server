b8653173ea6472334ea6a26320ee9c0beea5626d
황종옥 <jongock@hwangjong-og-ui-MacBookPro.local>
2019-11-06 16:39:42 +0900

    get API card inner join  modify

--- models/containers.js
+++ models/containers.js
@@ -2,13 +2,13 @@
 
 module.exports = (sequelize, DataTypes) => {
   const containers = sequelize.define('containers', {
-    b_key: {
+    ct_key: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
-    b_name: {
+    ct_name: {
       type: DataTypes.STRING(30),
       allowNull: false,
     },
