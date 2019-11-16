178b714f8e692b9029eb5270ca49423fdf89a7df
황종옥 <jongock@hwangjong-og-ui-MacBookPro.local>
2019-11-16 16:57:20 +0900

    Modify alert message

--- sever/routes/user.js
+++ sever/routes/user.js
@@ -6,6 +6,16 @@ var jwtKey = require("../config/jwt.js");
 
 var users = require("../../models").users
 
+router.get("/token", async (req,res) => {
+  console.log(req.token)
+  await users
+    .findOne({
+      where: {u_email: req.token.email},
+      attributes: ["u_name"]  
+    })
+    .then(result => res.send(result))
+    .catch(err => console.log(err))
+})
 
 router.post("/", async (req, res) => {
   const userExist = await users
@@ -52,13 +62,13 @@ router.post("/signin", async (req, res) => {
           if(key.toString('base64') === user.dataValues.u_password){
             res.cookie("user", token)
             res.json({
+              u_name: user.dataValues.u_name,
               success: true,
               message: '로그인 되었습니다.😋',
               token: token
             })
           } else {
             res.json({
-              u_name: user.dataValues.u_name,
               success: false,
               message: '회원정보가 일치 하지 않습니다.😰'
             });
@@ -77,39 +87,96 @@ router.post("/signout", (req, res) => {
 
 })
 
-router.put("/:id", (req, res) => {
-  if(req.token){
-    crpyto.randomBytes(64, (err, buf) => {
-      crpyto.pbkdf2(req.body.password, buf.toString('base64'), 10000, 64, 'sha512', (err,key)=>{
-        users.update({
-          u_name: req.body.name,
-          u_email: req.body.email,
-          u_password: key.toString("base64"),
-          salt: buf.toString("base64")
-        },
-        {
-          where: {
-            id: req.token.email
+router.post("/checkUser", async (req, res) => {
+  users
+    .findOne({ where : {u_email: req.token.email}})
+    .then(user => {
+      crpyto.pbkdf2(
+        req.body.password, user.dataValues.salt, 10000, 64, 'sha512', (err, key) => {
+          if(key.toString('base64') === user.dataValues.u_password){
+            res.json({
+              success: true,
+              message: '인증 되었습니다.😋',
+            })
+          } else {
+            res.json({
+              u_name: user.dataValues.u_name,
+              success: false,
+              message: '비밀번호를 확인해 주세요.😰'
+            });
           }
-        }
-        ).catch((err) => res.send(err))
-      })
+        })
     })
+    .catch((err) => {
+      res.send(400).json({
+        success: false,
+        message: '잘못된 요청입니다.😡'
+      });
+    })
+})
+router.put("/", async (req, res) => {
+  console.log(req.body.name)
+  if(req.token){
+    if(req.body.password){
+      crpyto.randomBytes(64, (err, buf) => {
+        crpyto.pbkdf2(req.body.password, buf.toString('base64'), 10000, 64, 'sha512', (err,key)=>{
+          users.update({
+            u_password: key.toString("base64"),
+            salt: buf.toString("base64")
+          },
+          {
+            where: {
+              u_email: req.token.email
+            }
+          })
+          .then(() => res.send({success: true, message: "수정이 완료 되었습니다.🙆🏻‍ 다시 로그인 해주세요!!"}))
+          .catch((err) => res.send(err))
+        })
+      })
+    }
+
+    if(req.body.name){
+     await users.update({
+        u_name: req.body.name,
+      },
+      {
+        where: {
+          u_email: req.token.email
+        }
+      })
+      .then(() => res.send({success: true, message: "수정이 완료 되었습니다.🙆🏻‍"}))
+      .catch((err) => res.send(err))
+    }
   }
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/', (req, res) => {
   if(req.token){
-    users.destroy({
-      where:{
-        u_email: req.token.email
-      }
-    })
-    .then(()=>{
-      res.json({
-        message: "탈퇴가 완료 되었습니다.",
-        success: true
-      })
+    users
+    .findOne({ where : {u_email: req.token.email}})
+    .then(user => {
+      crpyto.pbkdf2(
+        req.body.password, user.dataValues.salt, 10000, 64, 'sha512', (err, key) => {
+          if(key.toString('base64') === user.dataValues.u_password){
+            users.destroy({
+              where:{
+                u_email: req.token.email
+              }
+            })
+            .then(()=>{
+              res.json({
+                message: "탈퇴가 완료 되었습니다.",
+                success: true
+              })
+            })
+          } else {
+            res.json({
+              u_name: user.dataValues.u_name,
+              success: false,
+              message: '비밀번호를 확인해 주세요.😰'
+            });
+          }
+        })
     })
   } else {
     res.send(404).json({
